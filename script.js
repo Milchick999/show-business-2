a4201d44888a13b9aa4f45e3adf949adfe2d019d
Zinaida Rudak <rudakzinaida@gmail.com>
2025-12-08 02:02:00 +0300

    Add responsive main section

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ const overlay = document.querySelector('.header__overlay');
 burger.addEventListener('click', () => {
   overlay.classList.toggle('active');
   burger.classList.toggle('active');
-});пш
+});
