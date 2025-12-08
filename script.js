8bd2c402d2fd65833892cfb51f1c7e790b1db2e8
Zinaida Rudak <rudakzinaida@gmail.com>
2025-12-08 16:22:37 +0300

    Fix scroll in burger-button and hover-effect in header menu

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ const overlay = document.querySelector('.header__overlay');
 burger.addEventListener('click', () => {
   overlay.classList.toggle('active');
   burger.classList.toggle('active');
+  document.body.classList.toggle('menu-open');
 });
